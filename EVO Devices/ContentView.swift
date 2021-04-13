69400d4ef038f77251a899f3613038c206937799
Ron Yip <ronskyip@gmail.com>
2021-04-13 05:18:56 -0700

    Initial Commit

---
+++ EVO Devices/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EVO Devices
+//
+//  Created by Ronald Yip on 4/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EVO Devices/EVO_DevicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EVO_DevicesApp.swift
+//  EVO Devices
+//
+//  Created by Ronald Yip on 4/13/21.
+//
+
+import SwiftUI
+
+@main
+struct EVO_DevicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
