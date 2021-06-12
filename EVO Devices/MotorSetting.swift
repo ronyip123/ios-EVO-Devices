42735514a578eb477ebe8ffbe8d77ca9b34aeb81
Ron Yip <ronskyip@gmail.com>
2021-06-12 07:15:03 -0700

    checked in MotorSettings view file

---
+++ EVO Devices/MotorSetting.swift
@@ -0,0 +1,20 @@
+//
+//  MotorSetting.swift
+//  EVO Devices
+//
+//  Created by Ronald Yip on 6/12/21.
+//
+
+import SwiftUI
+
+struct MotorSetting: View {
+    var body: some View {
+        Text("Hello Git")
+    }
+}
+
+struct MotorSetting_Previews: PreviewProvider {
+    static var previews: some View {
+        MotorSetting()
+    }
+}
