c2df3629f51635965ded8f84a760f4ac10dc27f8
Ronald Yip <ronskyip@gmail.com>
2024-10-29 14:26:29 -0700

    Corrected the content of Background Alarm Scanning to show under construction

--- EVO Devices/BackgroundAlarmTaskSettings.swift
+++ EVO Devices/BackgroundAlarmTaskSettings.swift
@@ -17,49 +17,64 @@ struct BackgroundAlarmTaskSettings: View {
 
     var body: some View {
         VStack{
-            Button(action:{
-                if (backgroundTaskEnabled)
-                {
-                    // disable background task
-                    
-                }
-                else
-                {
-                    // request notification authorization
-//                    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-//                        if success {
-//                            print("Notification is all set!")
-//                        } else if let error = error {
-//                            print(error.localizedDescription)
-//                        }
-//                    }
-                                
-                    // enable background task
-                    let request = BGAppRefreshTaskRequest(identifier: backgroundAlarmTaskId) // Mark 1
-                    request.earliestBeginDate = Calendar.current.date(byAdding: .second, value: scanInterval, to: Date()) // Mark 2
-                    do {
-                        try BGTaskScheduler.shared.submit(request) // Mark 3
-                        print("Background Task Scheduled!")
-                    }
-                    catch(let error)
-                    {
-                        print("Scheduling Error \(error.localizedDescription)")
-                    }
-                }
-            })
-            {
-                if (backgroundTaskEnabled)
-                {
-                    Text("disable background task")
-                }
-                else
-                {
-                    Text("enable background task")
-                }
+            HStack {
+                Image(uiImage: UIImage(named: "AppIcon") ?? UIImage())
+                Text("Evolution Controls Inc.")
+                    .font(.title)
             }
-            .buttonStyle(.bordered)
-            .tint(.red)
-            .padding()
+            Text("EVO Devices")
+                .padding()
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            
+                Text("Background Alarm Scanning")
+                    .font(.title)
+        
+                Text("Under Construction")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        
+//            Button(action:{
+//                if (backgroundTaskEnabled)
+//                {
+//                    // disable background task
+//                    
+//                }
+//                else
+//                {
+//                    // request notification authorization
+////                    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+////                        if success {
+////                            print("Notification is all set!")
+////                        } else if let error = error {
+////                            print(error.localizedDescription)
+////                        }
+////                    }
+//                                
+//                    // enable background task
+//                    let request = BGAppRefreshTaskRequest(identifier: backgroundAlarmTaskId) // Mark 1
+//                    request.earliestBeginDate = Calendar.current.date(byAdding: .second, value: scanInterval, to: Date()) // Mark 2
+//                    do {
+//                        try BGTaskScheduler.shared.submit(request) // Mark 3
+//                        print("Background Task Scheduled!")
+//                    }
+//                    catch(let error)
+//                    {
+//                        print("Scheduling Error \(error.localizedDescription)")
+//                    }
+//                }
+//            })
+//            {
+//                if (backgroundTaskEnabled)
+//                {
+//                    Text("disable background task")
+//                }
+//                else
+//                {
+//                    Text("enable background task")
+//                }
+//            }
+//            .buttonStyle(.bordered)
+//            .tint(.red)
+//            .padding()
         }
         .onAppear(){
             //check if background task is running
